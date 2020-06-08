ad99ffc18193e9e2101dff52559b20f7321102ea
MatheusHAS <mathkeka@gmail.com>
2020-06-08 10:29:39 -0300

    refatoração 1

--- principal.py
+++ principal.py
@@ -4,21 +4,8 @@ from validacao import *
 # FEITO INTEGRAÇÃO COM DB
 # PRÓXIMO PASSO: REALIZAR VALIDAÇÕES#
 
-
-def menu():
-    print('1 - Cadastrar pessoa')
-    print('2 - editar pessoa')
-    print('3 - deletar pessoa')
-    print('4 - mostrar pessoas cadastradas')
-    print('0 - encerrar programa')
-
-
-def inicio():
-    print('=-'*40)
-    print('-------------------------------------INÍCIO-------------------------------------')
-    print('=-'*40)
-    print('\nEste programa é um simples CRUD de pessoas para efeito de treinamento:\n')
-
+inicio()
+menu()
 
 continua = True
 
@@ -26,16 +13,12 @@ while continua:
     inicio()
     menu()
 
-    escolha = str(
-        input('\nEscolha uma das opções acima para continuar com o programa: ')).strip().upper()
-    while escolha != '1' and escolha != '2' and escolha != '3' and escolha != '4' and escolha != '0':
-        escolha = str(
-            input('\nEscolha uma válida para continuar com o programa! tente novamente: ')).strip().upper()
+    escolha = escolhaMenu()
 
     if escolha == '1':
         # Instancia pessoa
-        pessoa = Pessoa(input('Digite o nome da pessoa: '), validaInt(), input(
-            'Digite o cpf da pessoa: '), input('Digite o celular da pessoa: '))
+        pessoa = Pessoa(inputString('nome'), validaInt('nova'),
+                        inputString('cpf'), inputString('celular'))
         # print(pessoa.__str__())
         inserirPessoaDB(pessoa)
         print('\nCadastro Efetuado corretamente!\n')
@@ -48,8 +31,8 @@ while continua:
 
         print('Mesmo que não deseje alterar um campo, escreva-o novamente. Do contrário, o campo ficará vazio.')
 
-        pessoa = Pessoa(input('Digite o novo nome da pessoa: '), validaInt('nova'), input(
-            'Digite o cpf da pessoa: '), input('Digite o novo celular da pessoa: '))
+        pessoa = Pessoa(inputString('nome'), validaInt('nova'),
+                        inputString('cpf'), inputString('celular'))
 
         editarPessoa(idPessEditar, pessoa)
         print('\nUsuário Editado!\n')
--- validacao.py
+++ validacao.py
@@ -8,3 +8,31 @@ def validaInt(txt=''):
             erro = True
             print('ERRO! digite apenas números inteiros!')
     return inteiro
+
+
+def inputString(txt):
+    texto = str(input(f'Digite o {txt} da pessoa: '))
+    return texto
+
+
+def escolhaMenu():
+    escolha = str(
+        input('\nEscolha uma das opções acima para continuar com o programa: ')).strip().upper()
+    while escolha != '1' and escolha != '2' and escolha != '3' and escolha != '4' and escolha != '0':
+        escolha = str(
+            input('\nEscolha uma válida para continuar com o programa! tente novamente: ')).strip().upper()
+    return escolha
+
+def menu():
+    print('1 - Cadastrar pessoa')
+    print('2 - editar pessoa')
+    print('3 - deletar pessoa')
+    print('4 - mostrar pessoas cadastradas')
+    print('0 - encerrar programa')
+
+
+def inicio():
+    print('=-'*40)
+    print('-------------------------------------INÍCIO-------------------------------------')
+    print('=-'*40)
+    print('\nEste programa é um simples CRUD de pessoas para efeito de treinamento:\n')
